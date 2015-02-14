8530e8dc9c7fc0e99ac1853745db5527c7319954
Paul Weatherby <paulweatherby@gmail.com>
2015-02-14 10:28:25 +0000

    so close i swear

--- Prototype/Phaser/Move_Player_with_JSI/RoBlockWar/Robot.js
+++ Prototype/Phaser/Move_Player_with_JSI/RoBlockWar/Robot.js
@@ -17,19 +17,21 @@ RoBlockWar_Robot.prototype.init = function(View){
 
 RoBlockWar_Robot.prototype.update = function(){
   
+  // Update location phaser -> model
   if(this.getRegister('X') != this.RobotPlayer.body.x ||
      this.getRegister('Y') != this.RobotPlayer.body.y)
   { console.log('player at (' + this.RobotPlayer.body.x + ', ' + this.RobotPlayer.body.y + ')'); }
-  
   this.setRegister('X', this.RobotPlayer.body.x);
   this.setRegister('Y', this.RobotPlayer.body.y);
   
-  if(this.getRegister('SPEEDX') != this.RobotPlayer.body.velocity.x ||
-     this.getRegister('SPEEDY') != this.RobotPlayer.body.velocity.y)
-  { console.log('player movement (' + this.RobotPlayer.body.velocity.x + ', ' + this.RobotPlayer.body.velocity.y + ')'); }
-  
-  this.RobotPlayer.body.velocity.x = this.getRegister('SPEEDX');
-  this.RobotPlayer.body.velocity.y = this.getRegister('SPEEDY');
+  // update velocity model -> phase
+  var newSpeedX = this.getRegister('SPEEDX') || 0;
+  var newSpeedY = this.getRegister('SPEEDY') || 0;
+  if(newSpeedX != this.RobotPlayer.body.velocity.x ||
+     newSpeedY != this.RobotPlayer.body.velocity.y)
+  { console.log('player movement (' + newSpeedX + ', ' + newSpeedY + ')'); }
+  this.RobotPlayer.body.velocity.x = newSpeedX;
+  this.RobotPlayer.body.velocity.y = newSpeedY;
   
   if(this.RobotPlayer.body.velocity.y > 0) {
     this.RobotPlayer.animations.play('up');
@@ -90,21 +92,26 @@ RoBlockWar_Robot.prototype.sleepFor = function( sleepDuration ){
 };
 
 RoBlockWar_Robot.prototype.setRegister = function(Reg, val){
-  console.log('Setting {' + Reg + '} to {' + val + '}');
+  //console.log('Setting {' + Reg + '} to {' + val + '}');
   switch (Reg) {
     case 'A':
+    case 1:
       this.Registers[1] = val;
       break;
     case 'X':
+    case 24:
       this.Registers[24] = val;
       break;
     case 'Y':
+    case 25:
       this.Registers[25] = val;
       break;
     case 'SPEEDX':
+    case 31:
       this.Registers[31] = val;
       break;
     case 'SPEEDY':
+    case 32:
       this.Registers[32] = val;
       break;
   }
@@ -114,64 +121,56 @@ RoBlockWar_Robot.prototype.getRegister = function(Reg){
   var val = null;
   switch (Reg) {
     case 'A':
+    case 1:
       val = this.Registers[1];
       break;
     case 'X':
+    case 24:
       val = this.Registers[24];
       break;
     case 'Y':
+    case 25:
       val = this.Registers[25];
       break;
     case 'SPEEDX':
+    case 31:
       val = this.Registers[31];
       return;
     case 'SPEEDY':
+    case 32:
       val = this.Registers[32];
       break;
   }
-  console.log('getting {' + Reg + '} to {' + val + '}');
+  // console.log('getting {' + Reg + '} currently {' + val + '}');
   return val;
 };
 
 function RoBlockWar_Robot_sampleCode() 
 {
-  
-  this.Registers[this.RegisterNameMapping.SPEEDX] = 0;
-  this.Registers[this.RegisterNameMapping.SPEEDY] = 0;
-  var times = 10;
-  while(times > 0)
-  {
-  	times = times - 1;
-  	while(this.Registers[this.RegisterNameMapping.X] < 50)
-  	{ 
-  		this.Registers[this.RegisterNameMapping.SPEEDX] = 50;
-  		this.sleepFor(1000);
-  	}
-  	this.Registers[this.RegisterNameMapping.SPEEDX] = 0;
-  	this.sleepFor(1000);
-  	
-  	while(this.Registers[this.RegisterNameMapping.Y] < 50)
-  	{ 
-  		this.Registers[this.RegisterNameMapping.SPEEDY] = 50;
-  		this.sleepFor(1000);
-  	}
-  	this.Registers[this.RegisterNameMapping.SPEEDY] = 0;
-  	this.sleepFor(1000);
-  	
-  	while(this.Registers[this.RegisterNameMapping.X] > 50)
-  	{ 
-  		this.Registers[this.RegisterNameMapping.SPEEDX] = -50;
-  		this.sleepFor(1000);
-  	}
-  	this.Registers[this.RegisterNameMapping.SPEEDX] = 0;
-  	this.sleepFor(1000);
-  	
-  	while(this.Registers[this.RegisterNameMapping.Y] > 50)
-  	{ 
-  		this.Registers[this.RegisterNameMapping.SPEEDY] = -50;
-  		this.sleepFor(1000);
-  	}
-  	this.Registers[this.RegisterNameMapping.SPEEDY] = 0;
-  	this.sleepFor(1000);
+  setRegister('SPEEDX', 0);
+  setRegister('SPEEDY', 0);
+  var times = 10000;
+  while(times > 0) {
+    times = times - 1;
+    while(getRegister('X') < 450) {
+      console.log('Trying to Move Right');
+      setRegister('SPEEDX', 350);
+    }
+    setRegister('SPEEDX', 0);
+    while(getRegister('Y') < 450) {
+      console.log('Trying to Move Down');
+      setRegister('SPEEDY', 350);
+    }
+    setRegister('SPEEDY', 0);
+    while(getRegister('X') > 50) {
+      console.log('Trying to Move Left');
+      setRegister('SPEEDX', -350);
+    }
+    setRegister('SPEEDX', 0);
+    while(getRegister('Y') > 50) {
+      console.log('Trying to Move Up');
+      setRegister('SPEEDY', -350);
+    }
+    setRegister('SPEEDY', 0);
   }
 }
